e58292235e0545bdac042719da07eccfec1357d2
Kumarjit Hazra <kumarjithazra43@gmail.com>
2024-08-05 21:44:24 +0530

    first commit

---
+++ asmd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"..........Enter two values below for addition,substraction,division and multiplication.........."<<endl;
+    cout<<"Enter First Number = ";
+    cin>>x;
+    cout<<"Now Enter Second Number = ";
+    cin>>y;
+    cout<<"The Addition Of " <<x<< " and "<<y<<" is = "<<x+y<<endl;
+    cout<<"The Substraction Of " <<x<< " and " <<y<<" is = "<<x-y<<endl;
+    cout<<"The Multiplication of "<<x<< " and "<<y<<" is = "<<x*y<<endl;
+    cout<<"The Division of "<<x<< " and " <<y<<" is = "<<x/y<<endl; 
+}
